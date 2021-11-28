7a4e48a233294e50370e79dbb0bf33dc8dbc7dc8
you990917 <you990917@mju.ac.kr>
2021-11-28 19:34:09 +0900

    changed

--- button1.py
+++ button1.py
@@ -7,6 +7,5 @@ b2 = Button(window, text="60161111")
 b3 = Button(window, text="hello")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
-b3.grid(row=1, column=0)
-
+b3.grid(row=1,column=0)
 window.mainloop()
